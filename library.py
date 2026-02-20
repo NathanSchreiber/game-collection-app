57ec6bfe426ffe920abbcc4c46c63a0f79d5b620
nschre11 <nschre11@wgu.edu>
2026-02-20 13:32:36 -0500

    Add the ability to remove games using the game's ID

--- library.py
+++ library.py
@@ -3,12 +3,19 @@ from game import Game
 class Library:
     def __init__(self):
         self.games = {}
+        self.id_list = []
 
     def add_game(self, new_game):
         self.games[new_game.id] = new_game
+        self.id_list.append(new_game.id)
+        return "Game added!"
 
     def remove_game(self, game_id):
-        pass
+        if game_id in self.games:
+            del self.games[game_id]
+            return "Game removed!"
+        else:
+            return("That game doesn't exist in your library")
 
     def __repr__(self):
         return str(self.games)
@@ -18,10 +25,10 @@ class Library:
         for game in self.games:
             entry = self.games[game]
             if entry.rating == None:
-                print_game = f"Title: {entry.title}, Platform: {entry.platform}, Finished: {entry.finished}, Playtime: {entry.playtime}, ID: {entry.id}"
+                print_game = f"Title: {entry.title}, Platform: {entry.platform}, Finished: {entry.finished}, Playtime: {entry.playtime} hrs, ID: {entry.id}"
                 print_game_list += f"{print_game}\n"
             else:
-                print_game = f"Title: {entry.title}, Platform: {entry.platform}, Finished: {entry.finished}, Rating: {entry.rating}/10 Playtime: {entry.playtime}, ID: {entry.id}"
+                print_game = f"Title: {entry.title}, Platform: {entry.platform}, Finished: {entry.finished}, Rating: {entry.rating}/10, Playtime: {entry.playtime} hrs, ID: {entry.id}"
                 print_game_list += f"{print_game}\n"
         return str(print_game_list)
 
@@ -31,4 +38,7 @@ library = Library()
 library.add_game(Game("The Outer Worlds", "Steam", True, None, 43))
 library.add_game(Game("Baldur's Gate 3", "Steam", True, 10, 180))
 
+print(library)
+
+library.remove_game(library.id_list[0])
 print(library)
